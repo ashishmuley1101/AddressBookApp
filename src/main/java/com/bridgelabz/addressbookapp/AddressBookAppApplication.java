c07836e6c07eb414fdb6357ac97453cbb98cfbbe
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-11-11 00:03:27 +0530

    Section 3 Lombok lib for logging using Slf4j annotation  UC2

--- src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
@@ -1,13 +1,18 @@
 package com.bridgelabz.addressbookapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// Use lombok Lib for Logging @Slf4j
+@Slf4j
 public class AddressBookAppApplication {
 
+    //log.info giving information on start screen
     public static void main(String[] args) {
         SpringApplication.run(AddressBookAppApplication.class, args);
+        log.info("Address Book App Started");
     }
 
 }
